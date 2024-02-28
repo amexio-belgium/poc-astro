3df2e5244a38fc6cf9df79a2e8059404cab074a7
Maarten Segers <MPParsley@users.noreply.github.com>
2024-02-28 16:01:34 +0000

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
-import axios from "axios";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  adapter: netlify()
 });
