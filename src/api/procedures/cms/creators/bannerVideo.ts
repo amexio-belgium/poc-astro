efc11773f7dcf0b2bd2e9966f8db4df2df9ad339
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-03-06 19:16:27 +0100

    Fix build errors, warnings

--- src/api/procedures/cms/creators/bannerVideo.ts
+++ src/api/procedures/cms/creators/bannerVideo.ts
@@ -1,13 +1,9 @@
-import {Components, type BannerVideo, type Button, CMSType, type BannerTiles} from '@trpc-procedures/cms/types.ts';
+import {Components, type BannerVideo} from '@trpc-procedures/cms/types.ts';
 import type {SharedBannerVideoComponent} from 'src/types/strapi/generated.schemas.ts';
 import type {
-    Maybe,
     MediaRemoteVideo,
-    MediaUnion,
-    ParagraphTeaser,
     ParagraphVideobanner
 } from 'src/types/drupal/resolvers-types.ts';
-import {getButtonsFromReference} from '@trpc-procedures/cms/helpers/button.ts';
 
 export function createBannerVideoStrapi(component: SharedBannerVideoComponent) {
     return {
