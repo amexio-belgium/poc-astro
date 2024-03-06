430614f27f15db82673aacaccf68ac09eda8e308
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-03-06 19:04:22 +0100

    Add breadcrumbs functionalitty for drupal

---
+++ src/api/procedures/cms/helpers/global.ts
@@ -0,0 +1,39 @@
+import {getHeaderDrupal} from '@trpc-procedures/cms/creators/page.ts';
+
+export type BreadcrumbItem = {
+    text: string;
+    href: string;
+}
+
+export async function getBreadCrumbsDrupal(slug: string, ssr:boolean){
+    let fullSlug = slug;
+    console.log(slug)
+    const header = await getHeaderDrupal();
+    let breadCrumbs: BreadcrumbItem[] = [];
+    
+    let foundAll = false;
+
+    while(!foundAll){
+        const element = header.elements.find(
+            (element)=>element.href === `${ssr? '/':''}${fullSlug}`
+        )
+        if(!element){
+            foundAll = true;
+        } else {
+            breadCrumbs.push(
+                {
+                    text: element.attributes.title!,
+                    href: `/drupal/${ssr ? '/ssr':''}${element.href}`,
+                }
+            );
+            let lastIndex = fullSlug.lastIndexOf("/");
+            if (lastIndex !== -1) {
+                fullSlug = fullSlug.substring(0, lastIndex);
+            }
+        }
+    }
+
+    breadCrumbs = breadCrumbs.reverse();
+    if(breadCrumbs.length-1>0) breadCrumbs[breadCrumbs.length-1].href = '';
+    return breadCrumbs;
+}
