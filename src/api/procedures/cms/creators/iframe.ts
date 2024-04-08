965f964d9542d194c794cc2ee3aa2cc9edc1220f
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-04-08 15:59:37 +0200

    Add iframe auto height

--- src/api/procedures/cms/creators/iframe.ts
+++ src/api/procedures/cms/creators/iframe.ts
@@ -2,7 +2,6 @@ import type {ParagraphIframe} from 'src/types/drupal/resolvers-types.ts';
 import {Components, type Iframe} from '@trpc-procedures/cms/types.ts';
 
 export function createIframeDrupal(component: ParagraphIframe){
-    console.log('return iframe')
     return {
         componentName: Components.Iframe,
         url: component.url.url
