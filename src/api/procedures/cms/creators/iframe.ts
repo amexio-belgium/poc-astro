5a48704d894ba4e64abb39e44330cf8614a1301a
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-04-08 10:50:51 +0200

    Add iframe component

--- src/api/procedures/cms/creators/iframe.ts
+++ src/api/procedures/cms/creators/iframe.ts
@@ -1,8 +1,10 @@
 import type {ParagraphIframe} from 'src/types/drupal/resolvers-types.ts';
-import type {Iframe} from '@trpc-procedures/cms/types.ts';
+import {Components, type Iframe} from '@trpc-procedures/cms/types.ts';
 
 export function createIframeDrupal(component: ParagraphIframe){
+    console.log('return iframe')
     return {
+        componentName: Components.Iframe,
         url: component.url.url
     } as Iframe
 }
