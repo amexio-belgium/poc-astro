78e1648ea50fd595f5453e79f48f6e8c116810b9
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-04-25 15:02:30 +0200

    Fix build errors

--- src/mapping/config/strapi/components/banner5050.mapping.factory.ts
+++ src/mapping/config/strapi/components/banner5050.mapping.factory.ts
@@ -1,35 +1,41 @@
-import type {SharedBanner5050Component, SharedButtonComponent} from "../../../../types/strapi/generated.schemas.ts";
+import type {SharedBanner5050Component, SharedButtonComponent} from "src/types/strapi/generated.schemas.ts";
 import type {
     Banner5050Mapping,
-    BannerCardMapping
-} from "../../../../models/components/banner5050/banner5050.mapping.ts";
+} from "src/models/components/banner5050/banner5050.mapping.ts";
 import {MappingFactory} from "../../../abstract/mapping.factory.ts";
-import type {ButtonMapping} from "../../../../models/components/button/button.mapping.ts";
+import type {Button} from 'src/models/components/button/button.interface.ts';
+import {ComponentsList} from 'src/models/super/components.type.ts';
 
 export class banner5050MappingFactory extends MappingFactory {
     createMapping(component: SharedBanner5050Component): Banner5050Mapping {
+        const strapiButton1 = component.buttons![0];
+        const strapiButton2 = component.buttons![1];
 
-        let cards: BannerCardMapping[] = [];
-        component.buttons?.splice(2);
+        const button1: Button = this.createButtonMapping(strapiButton1);
+        
+        const button2: Button = this.createButtonMapping(strapiButton2);
+        
 
-        component.buttons?.forEach(button => {
-            const card: BannerCardMapping = {
-                image: { url: button.image?.data?.attributes?.url || '' },
-                title: button.title || '',
-                description: button.description || '',
-                button: this.createButtonMapping(button)
-            }
-
-            cards.push(card);
-        });
-
-        return { cards };
+        return {buttons: [button1,button2]} ;
     }
 
-    createButtonMapping(button: SharedButtonComponent): ButtonMapping {
+    createButtonMapping(button: SharedButtonComponent): Button {
         return {
-            url: button.externalUrl || '',
-            text: button.buttonText || ''
+            componentName: ComponentsList.Button,
+            title: button.title!,
+            url: button.link?.data !== null ? button.link!.data!.attributes?.slug : button.externalUrl,
+            description: button.description,
+            image: {
+                name: button.image?.data?.attributes?.name ?
+                    button.image?.data?.attributes?.name
+                    :
+                    '',
+                url: button.image?.data?.attributes?.url ?
+                    import.meta.env.STRAPI_URL.concat(<string>button.image?.data?.attributes?.url)
+                    :
+                    ''
+            },
+            buttonText: button.buttonText
         }
     }
 }
--- src/models/components/button/button.model.ts
+++ src/models/components/button/button.model.ts
@@ -1,14 +1,23 @@
 import type { ButtonMapping } from "./button.mapping.ts";
 import type { Button } from "./button.interface.ts";
 import {ComponentsList} from "../../super/components.type.ts";
+import {ButtonColors, type Image} from '@trpc-procedures/cms/types.ts';
 
 export class ButtonModel implements Button {
     componentName = ComponentsList.Button;
+    title: string;
     url: string;
-    text: string;
+    description?: string;
+    color?: ButtonColors;
+    image?: Image;
+    buttonText?: string
 
     constructor (button: ButtonMapping) {
+        this.title = button.title;
         this.url = button.url;
-        this.text = button.text;
+        this.buttonText = button.text;
+        this.description = button.description;
+        this.color = button.color;
+        this.image = button.image;
     }
 }
