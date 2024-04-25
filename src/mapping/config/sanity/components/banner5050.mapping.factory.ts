2f2fb643cdb3eb9bf1ddb62cdeb794873ec8a07d
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-04-25 14:00:23 +0200

    add bannervideo and banner5050 in sanity

---
+++ src/mapping/config/sanity/components/banner5050.mapping.factory.ts
@@ -0,0 +1,16 @@
+import type {BannerVideoMapping} from "src/models/components/bannerVideo/bannerVideo.mapping.ts";
+import {toHTML} from '@portabletext/to-html'
+import {MappingFactory} from "../../../abstract/mapping.factory.ts";
+import type {BannerVideo} from 'src/types/sanity/sanity.types.ts';
+
+export class bannerVideoMappingFactory extends MappingFactory {
+
+    createMapping(component: BannerVideo): BannerVideoMapping {
+        const description = toHTML(component.description!);
+        return {
+            title: component.title!,
+            description,
+            url: component.video!.id!
+        }
+    }
+}
